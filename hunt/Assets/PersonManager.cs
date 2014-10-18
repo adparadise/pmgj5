8ee5c4df203260a00663762afd32ee4ccdd63a58
Michael DiRienzo <mdirienzo@cantina.co>
2014-10-18 14:30:09 -0400

    added the scene, and a person object.

---
+++ hunt/Assets/PersonManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		Vector2 speed = new Vector3(1, 0, 0);
+		Vector2 next = rigidbody2D.position + speed * Time.deltaTime;
+		rigidbody2D.MovePosition(next);	
+	}
+}
