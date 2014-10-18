bc65b963df2b9cbf919f2bcc5f69a8c2d6258f10
Andrew Paradise <adparadise@gmail.com>
2014-10-18 13:39:14 -0400

    a moving object

---
+++ hunt/Assets/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	private Vector2 speed = new Vector3(1, 0, 0);
+	void FixedUpdate() {
+		Vector2 next = rigidbody2D.position + speed * Time.deltaTime;
+		rigidbody2D.MovePosition(next);	
+	}
+}
