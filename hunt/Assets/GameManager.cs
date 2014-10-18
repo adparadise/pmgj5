43b6a53ea1892c3a4b856effed5fd9debe8668f0
Michael DiRienzo <mdirienzo@cantina.co>
2014-10-18 15:14:11 -0400

    Added player spawn logic.

--- hunt/Assets/GameManager.cs
+++ hunt/Assets/GameManager.cs
@@ -10,8 +10,14 @@ public class GameManager : MonoBehaviour {
 	public List<GameObject> peopleRefs;
 	public GameObject player;
 
+
+	// Prefabs.
+	public GameObject playerPrefab;
+	public GameObject personPrefab;
+
 	public void Awake() {
 		this.peopleRefs = new List<GameObject>();
+
 		
 		if (instance == null) {
 			instance = this;
@@ -24,13 +30,23 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		this.spawnPlayers();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+	private void spawnPlayers() {
+		Debug.Log("Spawning player!");
+
+		this.player = (GameObject)Instantiate(playerPrefab);
+		player.transform.position = new Vector3(0,0,0);
+
+	}
+
 }
 	
 	
