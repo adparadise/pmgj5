3ff40e4a9f1048ec723e8987b1a87d5ce7a540eb
Michael DiRienzo <mdirienzo@cantina.co>
2014-10-18 14:43:36 -0400

    Added a game manager.

---
+++ hunt/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public List<GameObject> peopleRefs;
+	public GameObject player;
+
+	public void Awake() {
+		this.peopleRefs = new List<GameObject>();
+		
+		if (instance == null) {
+			instance = this;
+		} else {
+			Debug.LogError("Only one copy of gamemanager allowed!");
+		}
+		
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+	
+	
+	
