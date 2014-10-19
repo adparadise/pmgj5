c1a0b05d9a92fa94d7a4cce6edcbdcafb3f5554c
Michael DiRienzo <mdirienzo@cantina.co>
2014-10-19 09:32:50 -0400

    actually add the timer and sound manager this time.

---
+++ hunt/Assets/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	private Time startTime;
+//	priva
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
