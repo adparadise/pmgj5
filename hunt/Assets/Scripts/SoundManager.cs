4f2ffd56a4a42a0f588083601510b38ce6a3e5e7
Michael DiRienzo <mdirienzo@cantina.co>
2014-10-19 13:44:22 -0400

    clean up the audio manager.

--- hunt/Assets/Scripts/SoundManager.cs
+++ hunt/Assets/Scripts/SoundManager.cs
@@ -22,17 +22,7 @@ public class SoundManager : MonoBehaviour {
 			Debug.LogError("Only one copy of gamemanager allowed!");
 		}
 
-		this.counterForNextClip = 0;
 
-//		this.audioSource = new AudioSource ();
-//		this.audioSource = GameObject.AddComponent<AudioSource> ();
-		this.audioSource.clip = music;
-//		this.audioSource.loop = true;
-
-		this.audioSource.Play ();
-//		audioSource = GetComponents<AudioSource> ();
-//		wasPlaying = new bool[audioSource.Length];
-//		audioSource[0].clip = music;
 	}
 	
 	// Update is called once per frame
@@ -55,29 +45,21 @@ public class SoundManager : MonoBehaviour {
 	}
 	
 	public void pause() {
-//		for (int i = 0; i < this.audioSource.Length; ++i) {
-//			AudioSource source = this.audioSource[i];
-//			this.wasPlaying[i] = source.isPlaying;
-//			if (source.isPlaying) source.Pause();
-//		}
 	}
 	
 	public void unpause() {
-//		for (int i = 0; i < this.audioSource.Length; ++i) {
-//			AudioSource source = this.audioSource[i];
-//			if (this.wasPlaying[i]) source.Play();
-//		}
+
 	}
 	
 	public void beginRound() {
-//		this.endRound();
-//		audioSource[0].Play();
+		this.audioSource.Stop ();
+		this.counterForNextClip = 0;
+		this.audioSource.clip = music;
+		this.audioSource.Play ();
 
 	}
 	
 	public void endRound() {
-//		audioSource[0].Stop();
-//		audioSource[0].volume = 1.0f;
 	}
 }
 
