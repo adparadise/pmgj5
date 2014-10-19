52688ff82bf3811b6520b0a7f23ff21334007f76
Andrew Paradise <adparadise@gmail.com>
2014-10-19 10:30:02 -0400

    add placeholder walk animations for player

---
+++ hunt/Assets/WalkCycleDirection.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkCycleDirection : MonoBehaviour {
+	
+	private Animator animator;
+	private Vector2 lastPos;
+	private int currentDirection = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		applyAnimationFromMovement ();
+	}
+
+	void applyAnimationFromMovement () {
+		Vector2 currentPos;
+		
+		currentPos = rigidbody2D.position;
+		if (this.lastPos == null) {
+			this.lastPos = currentPos;
+		}
+		Vector2 currentVector = new Vector2 (lastPos.x - currentPos.x, lastPos.y - currentPos.y);
+		this.lastPos = currentPos;
+
+		float currentDistance = currentVector.magnitude;
+		
+		bool isMoving = false;
+		if (currentDistance > 0.1) {
+			isMoving = true;
+		}
+
+		bool isYDominant = Mathf.Abs (currentVector.y) > Mathf.Abs (currentVector.x);
+		int direction;
+		if (isYDominant) {
+			if (currentVector.y > 0) {
+				//direction = "north";
+				direction = 0;
+			} else {
+				//direction = "south";
+				direction = 2;
+			}
+		} else {
+			if (currentVector.x > 0) {
+				//direction = "east";
+				direction = 1;
+			} else {
+				//direction = "west";
+				direction = 3;
+			}
+		}
+
+		if (isMoving) {
+			this.currentDirection = direction;
+		}
+	
+		setAnimationState (this.currentDirection, isMoving);
+	}
+
+	void setAnimationState (int direction, bool isMoving) {
+		animator.SetInteger("Direction", direction);
+		animator.SetBool ("IsMoving", isMoving);
+	}
+}
